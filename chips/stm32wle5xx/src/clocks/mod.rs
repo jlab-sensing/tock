3a12efd7bf6824bc8948e58c9fc1acc99e7b3fbf
Tyler Potyondy <potyondyt@gmail.com>
2025-12-18 09:30:53 -0800

    chips: stm32wle5xx: cleanup clock impl
    This adds doc comments and removes unused tests.

--- chips/stm32wle5xx/src/clocks/mod.rs
+++ chips/stm32wle5xx/src/clocks/mod.rs
@@ -1,8 +1,14 @@
 // Licensed under the Apache License, Version 2.0 or the MIT License.
 // SPDX-License-Identifier: Apache-2.0 OR MIT
-// Copyright OxidOS Automotive SRL.
-//
-// Author: Ioan-Cristian CÎRSTEA <ioan.cirstea@oxidos.io>
+// Copyright Tock Contributors 2025.
+
+//! The clock module for STM32WLE5xx chips.
+//!
+//! This is highly similar to the one for STM32L4xx chips. This clock
+//! implementation provides the minimal functionality required to enable
+//! peripherals and configure speeds (as tested for I2C and UART). This
+//! is still highly a work in progress and documentation comments here
+//! describing the usage will be updated as development continues.
 
 pub mod clocks;
 pub mod hse;
