3c5b7574abad0b39df6e4e1fc1e4b151dbd631ac
John Madden <jmadden173@pm.me>
2026-05-29 11:38:10 -0700

    Prepush fixes

--- boards/lora_e5_mini/src/main.rs
+++ boards/lora_e5_mini/src/main.rs
@@ -422,7 +422,9 @@ pub unsafe fn main() {
     });
 
     base_peripherals.i2c2.enable_clock();
-    base_peripherals.i2c2.set_speed(stm32wle5jc::i2c::I2CSpeed::Speed400k);
+    base_peripherals
+        .i2c2
+        .set_speed(stm32wle5jc::i2c::I2CSpeed::Speed400k);
     let i2c_master = components::i2c::I2CMasterDriverComponent::new(
         board_kernel,
         capsules_core::i2c_master::DRIVER_NUM,
