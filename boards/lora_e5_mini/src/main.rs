5132431e7f597b1dfe7bb72013747970fe14048e
John Madden <jmadden173@pm.me>
2026-06-24 09:21:43 -0700

    Reduce stack size

--- boards/lora_e5_mini/src/main.rs
+++ boards/lora_e5_mini/src/main.rs
@@ -64,7 +64,7 @@ const LORA_GPIO_DRIVER_NUM: usize = capsules_core::driver::NUM::LoRaPhyGPIO as u
 /// Dummy buffer that causes the linker to reserve enough space for the stack.
 #[no_mangle]
 #[link_section = ".stack_buffer"]
-pub static mut STACK_MEMORY: [u8; 0x2000] = [0; 0x2000];
+pub static mut STACK_MEMORY: [u8; 0x1500] = [0; 0x1500];
 
 /// A structure representing this platform that holds references to all
 /// capsules for this platform.
